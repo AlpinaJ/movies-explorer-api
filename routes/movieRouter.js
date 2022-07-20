4a3cae3e4382a68a6f69e9df438b8c82f2fb3814
julia953197 <j.andryukova@gmail.com>
2022-07-20 20:27:37 +0300

    Bugfix 10

--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -4,17 +4,17 @@ const { createMovie, getMovies, deleteMovie } = require("../controllers/movieCon
 
 movieRoutes.post("/", celebrate({
   body: Joi.object().keys({
-    country: Joi.string().required(),
-    director: Joi.string().required(),
-    duration: Joi.number().required(),
-    year: Joi.number().required(),
-    description: Joi.string().required(),
-    image: Joi.string().required().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
-    trailerLink: Joi.string().required().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
-    movieId: Joi.number().required(),
-    nameRU: Joi.string().required(),
-    nameEN: Joi.string().required(),
-    thumbnail: Joi.string().required(),
+    // country: Joi.string().required(),
+    // director: Joi.string().required(),
+    // duration: Joi.number().required(),
+    // year: Joi.number().required(),
+    // description: Joi.string().required(),
+    // image: Joi.string().required().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
+    // trailerLink: Joi.string().required().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
+    // movieId: Joi.number().required(),
+    // nameRU: Joi.string().required(),
+    // nameEN: Joi.string().required(),
+    // thumbnail: Joi.string().required(),
   }),
 }), createMovie);
 movieRoutes.get("/", getMovies);
