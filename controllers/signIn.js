8eb8fe4e892f4e92eeca7ce2e005b06453e0b061
julia953197 <j.andryukova@gmail.com>
2022-07-20 07:14:22 +0300

    Bugfix 7

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -30,7 +30,8 @@ module.exports.login = (req, res, next) => {
             console.log("token", token);
             res.cookie(JWT_KEY, token, JWT_OPTIONS);
             res.status(200).send({ message: "success" });
-            console.log(res.cookie);
+            console.log("cookie: ", res.cookie);
+            console.log("cookies: ", res.cookies);
           } else {
             next(new UnauthorizedError("Неправильные почта или пароль"));
           }
